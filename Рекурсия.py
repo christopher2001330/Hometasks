fb86d8a6eb5231fc1f96c50b370f6472e63dd995
christopher2001330 <kuk.khristofor@inbox.ru>
2024-08-12 20:19:38 +0300

    Now

---
+++ Рекурсия.py
@@ -0,0 +1,24 @@
+# # Функция вызывает саму себя
+# def summa(n):
+#     if n == 0:
+#         return 0
+#     else:
+#         return n + summa(n-1)
+#
+# # Стек - способ организации работы компьютера (LIFO) - последний добавленный элемент будет взят первым
+#
+# print(summa(5))
+
+# stack = []
+# stack.append(1)
+# print("Добавили элемент" , stack)
+# stack.append(2)
+# print("Добавили элемент" , stack)
+# stack.append(3)
+# print("Добавили элемент" , stack)
+# print(stack)
+# stack.pop()
+# print("Убрали элемент", stack)
+# stack.pop()
+# print("Убрали элемент", stack)
+
