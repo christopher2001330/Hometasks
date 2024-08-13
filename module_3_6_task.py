46579fc323dc63a2da243f6ccad1436d566a198b
christopher2001330 <kuk.khristofor@inbox.ru>
2024-08-13 15:30:31 +0300

    Now

--- module_3_6_task.py
+++ module_3_6_task.py
@@ -24,7 +24,11 @@ def calculate_structure_sum(structure):
         for key, value in structure.items():
             total_sum += calculate_structure_sum(value) # вытаскиваем значения, считаем их выше
             total_sum += calculate_structure_sum(key) # вытаскиваем значения, считаем их выше
+
+    
     return total_sum
 
 result = calculate_structure_sum(data_structure)
 print(result)
+
+# С первыми двумя if помог чат джипити. Я не понимаю как эти строчки считают значения, ведь там только return???
